efea56c3b482c62fa72b860dfbbd23f9c7cc33a6
guilherme <guilherme.c.silva18@aluno.senai.br>
2026-09-04 16:51:36 -0300

    e

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,27 @@
 const abrir = document.getElementById("contratar");
-const fechar = document.getElementById("fecharModal");
-const modal = document.getElementById("modal");
+const fechar = document.getElementById("fecharModal")
+const modal = document.getElementById("modal")
 const btnenviar = document.getElementById('btn-enviar')
 
 abrir.addEventListener("click", function() {
-    modal.style.display = "flex";
+    modal.style.display = "flex"
 });
 
 fechar.addEventListener("click", function() {
-    modal.style.display = "none";
+    modal.style.display = "none"
 });
 
 btnenviar.addEventListener('click', (e) => {
     e.preventDefault()
-    alert("Enviando com sucesso")
-    modal.style.display = "none"
+    if (condition) {
+        
+        alert("Enviando com sucesso")
+        modal.style.display = "none"
+    }
+
+    else {
+        alert("Falha")
+        modal.style.display = "flex"
+    }
 
 })
