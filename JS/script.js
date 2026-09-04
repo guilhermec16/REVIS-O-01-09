6fbd90f37b655d46b573aa1b84b4284c9bcec50f
guilherme <guilherme.c.silva18@aluno.senai.br>
2026-09-04 15:37:52 -0300

    sdfg

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+const abrir = document.getElementById("contratar");
+const fechar = document.getElementById("fecharModal");
+const modal = document.getElementById("modal");
+const btnenviar = document.getElementById('btn-enviar')
+
+abrir.addEventListener("click", function() {
+    modal.style.display = "flex";
+});
+
+fechar.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+btnenviar.addEventListener('click', (e) => {
+    e.preventDefault()
+    alert("Enviando com sucesso")
+    modal.style.display = "none"
+
+})
